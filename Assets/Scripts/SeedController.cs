2d4241a92f31564f2648089d13cb08e1e87cca52
maryam17 <marya.17.nov@gmail.com>
2019-08-14 16:17:16 +0200

    Added test tree models

--- Assets/Scripts/SeedController.cs
+++ Assets/Scripts/SeedController.cs
@@ -31,7 +31,7 @@ public class SeedController : MonoBehaviour
             Object.Destroy(this.gameObject);
 
             GameObject treeInstance = Instantiate(tree, treePosition, Quaternion.identity);
-            StartCoroutine(Grow(treeInstance));
+            Grow(treeInstance);
             //treeInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
@@ -41,12 +41,21 @@ public class SeedController : MonoBehaviour
        
     }
 
-    IEnumerator Grow(GameObject newTree)
+    void Grow(GameObject treeToGrow)
     {
-        for (float scale = 0f; scale >= maxHeight; scale += growthRate)
+        while (treeToGrow.transform.localScale.y <= maxHeight)
         {
-            newTree.transform.localScale += new Vector3(scale/3, scale, scale/3);
-            yield return null;
+            treeToGrow.transform.localScale = new Vector3(0.5f, treeToGrow.transform.localScale.y + growthRate, 1.0f);
         }
     }
+
+    //IEnumerator Grow(GameObject newTree)
+    //{
+    //    for (float scale = 0f; scale >= maxHeight; scale += growthRate)
+    //    {
+    //        newTree. += growthRate;
+    //        newTree.transform.localScale += new Vector3(scale/3, scale, scale/3);
+    //        yield return null;
+    //    }
+    //}
 }
---
+++ Assets/TreeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+
+    public float maxHeight;
+    public float growthRate;
+    public float growthTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHeight = 1.0f;
+        growthRate = 2.0f;
+        growthTime = 4.0f;
+
+        this.gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 oldScale = this.gameObject.transform.localScale;
+        //Vector3 newScale = new Vector3(0.5f, oldScale * 2, )
+        this.gameObject.transform.localScale = Vector3.Lerp(oldScale, oldScale * growthRate, growthTime * Time.deltaTime);
+    }
+}
